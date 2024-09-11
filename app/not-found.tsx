a59df46dbd507d732528ac5e04033c78107963ee
Daniel Murawsky <d.r.murawsky@gmail.com>
2024-09-11 15:31:12 -0400

    save

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,5 +1,9 @@
 import { NotFoundForm } from "@/components/not-found-form";
 
 export default function NotFound() {
-  return <NotFoundForm />;
+  return (
+    <div className="p-2">
+      <NotFoundForm />
+    </div>
+  );
 }
